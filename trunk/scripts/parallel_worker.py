c13b31538104c3aff8ee448a271774be3e9c35e3
schmidtga00@gmail.com <schmidtga00@gmail.com@9f6fdac3-a0f0-116b-f226-14b5c2d388a4>
2013-05-22 13:38:54 +0000

    Current parallel version of the BA code with the HDFtoTIFF section and the create seasonal summaries sections in parallel.  The HDFtoTIFF reads one line at at time and does the conversion.  The spectral indices is also processing one line at a time.  The change from scene-based processing to line-based processing allows solid speed-up as the processes are no longer I/O bound.

--- trunk/scripts/parallel_worker.py
+++ trunk/scripts/parallel_worker.py
@@ -4,7 +4,7 @@ import time
 from log_it import *
 from process_temporal_ba_stack import temporalBAStack
  
-class parallelWorker(multiprocessing.Process):
+class parallelSceneWorker(multiprocessing.Process):
  
     def __init__ (self, work_queue, result_queue, stackObject):
         # base class initialization
@@ -31,6 +31,40 @@ class parallelWorker(multiprocessing.Process):
             status = self.stackObject.sceneHDFToTiff (hdf_file)
             if status != SUCCESS:
                 msg = 'Error converting the HDF file (%s) to GeoTIFF. ' \
+                    'Processing will terminate.' % hdf_file
+                logIt (msg, self.stackObject.log_handler)
+ 
+            # store the result
+            self.result_queue.put(status)
+
+
+class parallelSummaryWorker(multiprocessing.Process):
+ 
+    def __init__ (self, work_queue, result_queue, stackObject):
+        # base class initialization
+        multiprocessing.Process.__init__(self)
+ 
+        # job management stuff
+        self.work_queue = work_queue
+        self.result_queue = result_queue
+        self.stackObject = stackObject
+        self.kill_received = False
+ 
+
+    def run(self):
+        while not self.kill_received:
+            # get a task
+            try:
+                year = self.work_queue.get_nowait()
+            except Queue.Empty:
+                break
+ 
+            # process the scene
+            print 'Processing year ' + str(year) + ' ...'
+            status = SUCCESS
+            status = self.stackObject.generateYearSeasonalSummaries (year)
+            if status != SUCCESS:
+                msg = 'Error processing seasonal summaries for year %d. '  \
                     'Processing will terminate.' % hdf_file
                 logIt (msg, self.stackObject.log_handler)
  
